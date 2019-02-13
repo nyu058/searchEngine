cdabf79bdf8ed09a9ff7ea1a92c32b6c2a8de685
Nathan Yu <nyu058@uottawa.ca>
2019-02-12 19:56:31 -0500

    boolean index

--- indexing/booleanIndexer.py
+++ indexing/booleanIndexer.py
@@ -1,14 +1,29 @@
-from .indexer import Indexer
+from indexing import indexer
 import os.path
 from dictionaryBuilding import dictionaryBuilding
 
-class BooleanIndexer(Indexer):
-    def __init__(self,):
+class BooleanIndexer(indexer.Indexer):
+    def __init__(self):
         super().__init__('boolean')
 
     def buildIndex(self, dictionary):
         boolean_index=[]
-        for tuple in self.parseIndex(dictionary):
+        sorted=self.parseIndex(dictionary)
+        i=0
+        j=0
+        while i <len(sorted):
+            while j<len(sorted):
+                if sorted[i][0]!=sorted[j][0]:
+                    boolean_index.append((sorted[i][0], j-i))
+                    i=j
+                    break
+                if j==len(sorted)-1:
+                    boolean_index.append((sorted[j][0], j-i+1))
+
+                    return boolean_index
+
+                j += 1
+            # print(boolean_index)
 
 
 
